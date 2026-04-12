d42f754348bd53c58726d7292fcf1e71de9b9a02
ignacio14.castillor@gmail.com <ignacio.castillo-r@mail.escuelaing.edu.co>
2026-04-12 12:27:50 -0500

    continuación laboratorio

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,25 +1,45 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.prefix}-vnet"
-  address_space       = ["10.10.0.0/16"]
+resource "azurerm_network_interface" "nic" {
+  count               = var.vm_count
+  name                = "${var.prefix}-nic-${count.index}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  tags                = var.tags
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+  tags = var.tags
 }
 
-resource "azurerm_subnet" "web" {
-  name                 = "subnet-web"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.10.1.0/24"]
-}
+resource "azurerm_linux_virtual_machine" "vm" {
+  count               = var.vm_count
+  name                = "${var.prefix}-vm-${count.index}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = "Standard_B1s"
 
-resource "azurerm_subnet" "mgmt" {
-  name                 = "subnet-mgmt"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.10.2.0/24"]
-}
+  admin_username = var.admin_username
+  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
 
-output "subnet_web_id" {
-  value = azurerm_subnet.web.id
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(var.cloud_init)
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  tags = var.tags
 }
